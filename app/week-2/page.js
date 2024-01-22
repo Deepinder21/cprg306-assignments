5ef4609a26b55f1e7582c22535eb746f4ef0fa3d
Deepinder21 <125156553+Deepinder21@users.noreply.github.com>
2024-01-22 06:06:19 -0700

    Added week-2 Folder

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import Name from './student-info.js';
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <Name></Name>
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Name () {
+    return (
+        <main>
+          <h1>Deepinder Singh Aulakh</h1>
+          <ul>
+            <li>
+                <Link href = "https://github.com/Deepinder21">Github</Link>
+            </li>
+          </ul>
+        </main>
+      );
+
+}
